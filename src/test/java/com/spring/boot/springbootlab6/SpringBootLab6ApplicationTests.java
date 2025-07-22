5e93cc849658e91fb4403d9249d0433928aab8cf
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-22 10:15:57 +0300

    Setting up git

---
+++ src/test/java/com/spring/boot/springbootlab6/SpringBootLab6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.springbootlab6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLab6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
