32ac112d5e1af6bacc30e0a238f4d4fdbd9a8980
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-22 10:20:58 +0300

    fixing git

---
+++ EmployeeManagementSystem/src/main/java/com/spring/boot/springbootlab6/Api/ApiResponse.java
@@ -0,0 +1,10 @@
+package com.spring.boot.springbootlab6.Api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+}
